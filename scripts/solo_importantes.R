c5b456a81d2451349aff9ad7888348bc56eec635
fedefliguer <56199576+fedefliguer@users.noreply.github.com>
2020-11-19 11:15:31 -0300

    Update solo_importantes.R

--- scripts/solo_importantes.R
+++ scripts/solo_importantes.R
@@ -1,5 +1,10 @@
-myfile <- "https://raw.githubusercontent.com/fedefliguer/maestria_DMEyF_TP02/main/importances/importances_max_1_5.csv"
-importances <- data.table(read_csv2(myfile))
-importances = importances[max_gan>importance_min]
-variables = unique(importances$Feature)
-dataset = dataset[, c("foto_mes", "numero_de_cliente", variables, "clase01"), with=FALSE]
+# Nombre de archivo: solo_importantes
+# Función: Toma las variables importantes de los modelos 1 a 5 y conserva aquellas que superen cierto nivel mínimo
+
+solo_importantes = function(dataset, importance_min){
+  myfile <- "https://raw.githubusercontent.com/fedefliguer/maestria_DMEyF_TP02/main/importances/importances_max_1_5.csv"
+  importances <- data.table(read_csv2(myfile))
+  importances = importances[max_gan>importance_min]
+  variables = unique(importances$Feature)
+  dataset = dataset[, c("foto_mes", "numero_de_cliente", variables, "clase01"), with=FALSE]
+  }
