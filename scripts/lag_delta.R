aac8648a943f2d80b2d98a807ba94938f11bfa3a
fedefliguer <56199576+fedefliguer@users.noreply.github.com>
2020-11-19 11:13:31 -0300

    Update lag_delta.R

--- scripts/lag_delta.R
+++ scripts/lag_delta.R
@@ -1,10 +1,15 @@
-campos_lags  <- setdiff(  colnames(dataset) ,  c("clase_ternaria","clase01", "numero_de_cliente","foto_mes") )
+# Nombre de archivo: lag_delta.R
+# Función: Sobre el dataset original, genera un lag y un delta
 
-setorderv( dataset, c("numero_de_cliente","foto_mes") )
-dataset[,  paste0( campos_lags, "_lag1") :=shift(.SD, 1, NA, "lag"), by=numero_de_cliente, .SDcols= campos_lags]
+lag_delta = function(dataset){
+  campos_lags  <- setdiff(  colnames(dataset) ,  c("clase_ternaria","clase01", "numero_de_cliente","foto_mes") )
 
-#agrego los deltas de los lags, de una forma nada elegante
-for( vcol in campos_lags )
-{
-  dataset[,  paste0(vcol, "_delta1") := get( vcol)  - get(paste0( vcol, "_lag1"))]
+  setorderv( dataset, c("numero_de_cliente","foto_mes") )
+  dataset[,  paste0( campos_lags, "_lag1") :=shift(.SD, 1, NA, "lag"), by=numero_de_cliente, .SDcols= campos_lags]
+
+  #agrego los deltas de los lags, de una forma nada elegante
+  for( vcol in campos_lags )
+  {
+    dataset[,  paste0(vcol, "_delta1") := get( vcol)  - get(paste0( vcol, "_lag1"))]
+  }
 }
