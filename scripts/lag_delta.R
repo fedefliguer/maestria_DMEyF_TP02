f891150cdacc3f521da3a3da7c019256948987c8
fedefliguer <56199576+fedefliguer@users.noreply.github.com>
2020-11-19 11:12:33 -0300

    Create lag_delta.R

---
+++ scripts/lag_delta.R
@@ -0,0 +1,10 @@
+campos_lags  <- setdiff(  colnames(dataset) ,  c("clase_ternaria","clase01", "numero_de_cliente","foto_mes") )
+
+setorderv( dataset, c("numero_de_cliente","foto_mes") )
+dataset[,  paste0( campos_lags, "_lag1") :=shift(.SD, 1, NA, "lag"), by=numero_de_cliente, .SDcols= campos_lags]
+
+#agrego los deltas de los lags, de una forma nada elegante
+for( vcol in campos_lags )
+{
+  dataset[,  paste0(vcol, "_delta1") := get( vcol)  - get(paste0( vcol, "_lag1"))]
+}
