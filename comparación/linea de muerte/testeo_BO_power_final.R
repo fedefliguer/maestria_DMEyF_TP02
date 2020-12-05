a20b95e56a30c1dfcde81e62033dfc4596abdbdf
fedefliguer <56199576+fedefliguer@users.noreply.github.com>
2020-12-05 14:52:27 -0300

    Update testeo_BO_power_final.R

--- comparación/linea de muerte/testeo_BO_power_final.R
+++ comparación/linea de muerte/testeo_BO_power_final.R
@@ -4,15 +4,77 @@
 rm( list=ls() )  #remove all objects
 gc()             #garbage collection
 
+# install.packages("primes")
 require("data.table")
 require("primes")
 require("lightgbm")
 require("DiceKriging")
 require("mlrMBO")
+library(devtools)
+library(readr)
 
 ksemilla_lgb  <-  102191
 
-karchivo_salida <- "~/buckets/b2/work/parametros_fijos_pasados.txt"
+karchivo_salida <- "parametros_fijos_pasados.txt"
+
+# Carga del ds 
+setwd("/home/fjf_arg_gcloud/buckets/b1/datasets/")
+
+kcampos_separador <-  "\t"
+karchivo_entrada <-  "paquete_premium.txt.gz"
+ds <- fread(karchivo_entrada, header=TRUE, sep=kcampos_separador)
+ds[ , clase01 :=  ifelse( clase_ternaria=="BAJA+2", 1L, 0L)  ]
+ds[ , clase_ternaria :=  NULL  ]
+
+# Ensayo 1: Corrección de columnas que a priori son malas
+source_url("https://raw.githubusercontent.com/fedefliguer/maestria_DMEyF_TP02/main/scripts/correcciones.R")
+correcciones(ds)
+
+# Ensayo 2: Nuevas columnas armadas por Fede
+source_url("https://raw.githubusercontent.com/fedefliguer/maestria_DMEyF_TP02/main/scripts/nuevas_columnas_fede.R")
+nuevas_columnas_fede(ds)
+
+# Ensayo 3: Nuevas columnas armadas por cátedra
+source_url("https://raw.githubusercontent.com/fedefliguer/maestria_DMEyF_TP02/main/scripts/nuevas_columnas_catedra.R")
+nuevas_columnas_catedra(ds)
+
+base = ds
+
+# Si quiero quedarme únicamente con las n variables más importantes según los modelos 1 a 5
+source_url("https://raw.githubusercontent.com/fedefliguer/maestria_DMEyF_TP02/main/scripts/solo_importantes.R")
+nu_variables = 150
+solo_importantes(ds, nu_variables)
+
+# Agrego el lag 1 y el delta 1 para todas las variables seleccionadas
+source_url("https://raw.githubusercontent.com/fedefliguer/maestria_DMEyF_TP02/main/scripts/lag_delta.R")
+lag_delta(ds)
+
+# Agrego históricas (maximo, minimo, tendencia) para las ventanas que quiera
+ventanas = c(12)
+source_url("https://raw.githubusercontent.com/fedefliguer/maestria_DMEyF_TP02/main/scripts/variables_historicas.R")
+variables_historicas(ds, ventanas)
+
+ds_150 = ds
+
+# SOLO PARA LAS 50 MEJORES
+ds = base
+
+source_url("https://raw.githubusercontent.com/fedefliguer/maestria_DMEyF_TP02/main/scripts/solo_importantes.R")
+nu_variables = 50
+solo_importantes(ds, nu_variables)
+
+ventanas = c(3, 6)
+source_url("https://raw.githubusercontent.com/fedefliguer/maestria_DMEyF_TP02/main/scripts/variables_historicas.R")
+variables_historicas(ds, ventanas)
+
+ds_50 = ds[ , grepl( "__" , names( ds ) ), with = FALSE ]
+
+dataset = cbind(ds_150, ds_50)
+
+rm(ds_150, ds_50)
+
+#los campos que se van a utilizar, intencionalmente no uso  numero_de_cliente
+campos_buenos  <- setdiff(  colnames(dataset) ,  c("clase_ternaria","clase01","numero_de_cliente") )
 
 #------------------------------------------------------------------------------
 
@@ -64,14 +126,6 @@ ganancia_public_pcorte  <- function(  x )
 
 modelo_fijo_en_periodo  <- function(  pmes_aplicacion, psemilla_undersampling )
 {
-  #Esta parte es Fool Proof
-  #Si esta fuera de rango, no hago nada
-  meses_validos <-  c(                201803, 201804, 201805, 201806, 201807, 201808, 201809, 201810, 201811, 201812,
-                                      201901, 201902, 201903, 201904, 201905, 201906, 201907, 201908, 201909, 201910, 201911, 201912,
-                                      202001, 202002, 202003 )
-  
-  if( !( pmes_aplicacion %in% meses_validos )  )  return()
-  
   
   vmes_last  <- restar_dos_meses( pmes_aplicacion )
   vmes_test  <- restar_un_ano( pmes_aplicacion )
@@ -83,7 +137,7 @@ modelo_fijo_en_periodo  <- function(  pmes_aplicacion, psemilla_undersampling )
   
   #defino el dataset donde voy a entrenar
   dataset[ ,  BO_train := 0L]
-  dataset[ ( foto_mes>=201701 & foto_mes<=vmes_last & foto_mes!=vmes_test & ( clase01==1 | azar<=0.05) ),  BO_train := 1L]
+  dataset[ ( foto_mes>=201701 & foto_mes<=vmes_last & foto_mes!=vmes_test & ( clase01==1 | azar<=0.25) ),  BO_train := 1L]
   
   #dejo los datos en el formato que necesita LightGBM
   dBO_train  <<- lgb.Dataset( data  = data.matrix(  dataset[ BO_train==1, campos_buenos, with=FALSE]),
@@ -110,12 +164,12 @@ modelo_fijo_en_periodo  <- function(  pmes_aplicacion, psemilla_undersampling )
                             metric= "custom",
                             num_iterations=  999999,
                             early_stopping_rounds=  200,
-                            learning_rate= # VALOR OBTENIDO
-                            min_data_in_leaf= as.integer( # VALOR OBTENIDO ),
+                            learning_rate = 0.08729865,
+                            min_data_in_leaf= as.integer( 7317.16421 ),
                             feature_pre_filter= FALSE,
-                            feature_fraction= # VALOR OBTENIDO,
-                            lambda_l1= # VALOR OBTENIDO,
-                            lambda_l2= # VALOR OBTENIDO,
+                            feature_fraction= 0.7547246,
+                            lambda_l1= 0.8124524018,
+                            lambda_l2= 68.419841,
                             verbose= -1,
                             seed= ksemilla_lgb
   )
@@ -175,7 +229,6 @@ modelo_fijo_en_periodo  <- function(  pmes_aplicacion, psemilla_undersampling )
          psemilla_undersampling,
          vsemilla,
          max( vganancias ) / 0.2,  #Ganancia maxima teorica alcanzable
-         as.integer( run$x$pmin_data_in_leaf ),
          as.integer(BO_pcorte$x$pcorte_entero)/100,  #probabilidad de corte
          ganancia_public/0.2,  #ganancia normalizada en leaderboard Public
          ganancia_private/0.8, #ganancia normalizada en leaderboard Public
@@ -194,69 +247,7 @@ modelo_fijo_en_periodo  <- function(  pmes_aplicacion, psemilla_undersampling )
 #------------------------------------------------------------------------------
 #------------------------------------------------------------------------------
 
-# Carga del ds 
-setwd("/home/antonellaalopez/buckets/b1/datasetsOri/")
-
-kcampos_separador               <-  "\t"
-karchivo_entrada_full     <-  "paquete_premium.txt.gz"
-ds <- fread(karchivo_entrada_full, header=TRUE, sep=kcampos_separador)
-
-
-ds[ , clase01 :=  ifelse( clase_ternaria=="BAJA+2", 1L, 0L)  ]
-ds[ , clase_ternaria :=  NULL  ]
-ds = ds[foto_mes>=201701 , ]
-
-
-# Ensayo 1: Corrección de columnas que a priori son malas
-source_url("https://raw.githubusercontent.com/fedefliguer/maestria_DMEyF_TP02/main/scripts/correcciones.R")
-correcciones(ds)
-
-# Ensayo 2: Nuevas columnas armadas por Fede
-source_url("https://raw.githubusercontent.com/fedefliguer/maestria_DMEyF_TP02/main/scripts/nuevas_columnas_fede.R")
-nuevas_columnas_fede(ds)
-
-# Ensayo 3: Nuevas columnas armadas por cátedra
-source_url("https://raw.githubusercontent.com/fedefliguer/maestria_DMEyF_TP02/main/scripts/nuevas_columnas_catedra.R")
-nuevas_columnas_catedra(ds)
-
-base = ds
-
-# Si quiero quedarme únicamente con las n variables más importantes según los modelos 1 a 5
-source_url("https://raw.githubusercontent.com/fedefliguer/maestria_DMEyF_TP02/main/scripts/solo_importantes.R")
-nu_variables = 150
-solo_importantes(ds, nu_variables)
-
-# Agrego el lag 1 y el delta 1 para todas las variables seleccionadas
-source_url("https://raw.githubusercontent.com/fedefliguer/maestria_DMEyF_TP02/main/scripts/lag_delta.R")
-lag_delta(ds)
-
-# Agrego históricas (maximo, minimo, tendencia) para las ventanas que quiera
-ventanas = c(12)
-source_url("https://raw.githubusercontent.com/fedefliguer/maestria_DMEyF_TP02/main/scripts/variables_historicas.R")
-variables_historicas(ds, ventanas)
-
-ds_150 = ds
-
-# SOLO PARA LAS 50 MEJORES
-ds = base
-
-source_url("https://raw.githubusercontent.com/fedefliguer/maestria_DMEyF_TP02/main/scripts/solo_importantes.R")
-nu_variables = 50
-solo_importantes(ds, nu_variables)
-
-ventanas = c(3, 6)
-source_url("https://raw.githubusercontent.com/fedefliguer/maestria_DMEyF_TP02/main/scripts/variables_historicas.R")
-variables_historicas(ds, ventanas)
-
-ds_50 = ds[ , grepl( "__" , names( ds ) ), with = FALSE ]
-
-dataset = cbind(ds_150, ds_50)
-
-rm(ds_150, ds_50)
-
-#los campos que se van a utilizar, intencionalmente no uso  numero_de_cliente
-campos_buenos  <- setdiff(  colnames(dataset) ,  c("clase_ternaria","clase01","numero_de_cliente") )
-
+setwd("/home/fjf_arg_gcloud/buckets/b1/tp2-working/work")
 
 #genero los titulos del archivo de salida
 if(!file.exists(karchivo_salida))
@@ -265,7 +256,6 @@ if(!file.exists(karchivo_salida))
        "semilla_undersampling",
        "semilla_public",
        "ganancia_public_inalcanzable",
-       "min_gain_to_split",
        "probabilidad_corte",
        "ganancia_public",
        "ganancia_private",
@@ -286,3 +276,97 @@ for(  pmes_aplicacion in  meses_validos )
 {
   modelo_fijo_en_periodo(  pmes_aplicacion, psemilla_undersampling )
 }
+
+############# AHORA APLICO A LA PREDICCIÓN
+
+#hago undersampling de los negativos
+set.seed(psemilla_undersampling)
+dataset[ , azar:= runif( nrow(dataset) ) ]
+
+dataset[ ,  BO_train := 0L]
+dataset[ ( foto_mes>=201701 & foto_mes<=202003 & foto_mes!=201905 & ( clase01==1 | azar<=0.25) ),  BO_train := 1L] ##### ACÁ SE PUEDE TAMBIÉN EXCLUIR LOS MESES QUE DIJO EN EL ULTIMO MAIL
+
+#dejo los datos en el formato que necesita LightGBM
+dBO_train  <<- lgb.Dataset( data  = data.matrix(  dataset[ BO_train==1, campos_buenos, with=FALSE]),
+                            label = dataset[ BO_train==1, clase01],
+                            free_raw_data = FALSE
+)
+
+
+#defino el dataset donde voy a testear
+dataset[ ,  BO_test1 := 0L]
+dataset[ foto_mes==201905,  BO_test1 := 1L]
+
+dBO_test1  <<- lgb.Dataset( data  = data.matrix(  dataset[ BO_test1==1, campos_buenos, with=FALSE]),
+                            label = dataset[ BO_test1==1, clase01],
+                            free_raw_data = FALSE
+)
+
+#calculo el modelo final que usa el valor optimo de tamaño de hoja encontrado antes
+modelo_final <- lgb.train(data= dBO_train,
+                          objective= "binary",
+                          eval= fganancia_logistic_lightgbm,
+                          valids= list( valid1= dBO_test1 ),
+                          first_metric_only= TRUE,
+                          metric= "custom",
+                          num_iterations=  999999,
+                          early_stopping_rounds=  200,
+                          learning_rate = 0.08729865,
+                          min_data_in_leaf= as.integer( 7317.16421 ),
+                          feature_pre_filter= FALSE,
+                          feature_fraction= 0.7547246,
+                          lambda_l1= 0.8124524018,
+                          lambda_l2= 68.419841,
+                          verbose= -1,
+                          seed= ksemilla_lgb
+)
+
+#este mes nunca se utilizo para entrenar
+dataset_aplicacion <<- copy( dataset[ foto_mes==202005, ] )
+
+#Genero los archivos que voy a probar contra el Leaderboard Publico y quedarme con el mejor
+prediccion_final  <- predict( modelo_final,  data.matrix( dataset_aplicacion[ , campos_buenos, with=FALSE]))
+
+entrega <-   as.data.table(cbind( "numero_de_cliente" = dataset_aplicacion[ foto_mes==202005, numero_de_cliente],  "prob" = prediccion_final) )
+entrega = entrega[order(-prob)]
+
+entrega[  ,  estimulo :=  c(rep(1, 6000),rep(0,nrow(entrega)-6000))]
+fwrite( entrega[ ,  c('numero_de_cliente', 'estimulo'), with=FALSE], sep=',',  file='entrega_power_corta_6000.csv')
+entrega[  ,  estimulo :=  c(rep(1, 6100),rep(0,nrow(entrega)-6100))]
+fwrite( entrega[ ,  c('numero_de_cliente', 'estimulo'), with=FALSE], sep=',',  file='entrega_power_corta_6100.csv')
+entrega[  ,  estimulo :=  c(rep(1, 6200),rep(0,nrow(entrega)-6200))]
+fwrite( entrega[ ,  c('numero_de_cliente', 'estimulo'), with=FALSE], sep=',',  file='entrega_power_corta_6200.csv')
+entrega[  ,  estimulo :=  c(rep(1, 6300),rep(0,nrow(entrega)-6300))]
+fwrite( entrega[ ,  c('numero_de_cliente', 'estimulo'), with=FALSE], sep=',',  file='entrega_power_corta_6300.csv')
+entrega[  ,  estimulo :=  c(rep(1, 6400),rep(0,nrow(entrega)-6400))]
+fwrite( entrega[ ,  c('numero_de_cliente', 'estimulo'), with=FALSE], sep=',',  file='entrega_power_corta_6400.csv')
+entrega[  ,  estimulo :=  c(rep(1, 6500),rep(0,nrow(entrega)-6500))]
+fwrite( entrega[ ,  c('numero_de_cliente', 'estimulo'), with=FALSE], sep=',',  file='entrega_power_corta_6500.csv')
+entrega[  ,  estimulo :=  c(rep(1, 6600),rep(0,nrow(entrega)-6600))]
+fwrite( entrega[ ,  c('numero_de_cliente', 'estimulo'), with=FALSE], sep=',',  file='entrega_power_corta_6600.csv')
+entrega[  ,  estimulo :=  c(rep(1, 6700),rep(0,nrow(entrega)-6700))]
+fwrite( entrega[ ,  c('numero_de_cliente', 'estimulo'), with=FALSE], sep=',',  file='entrega_power_corta_6700.csv')
+entrega[  ,  estimulo :=  c(rep(1, 6800),rep(0,nrow(entrega)-6800))]
+fwrite( entrega[ ,  c('numero_de_cliente', 'estimulo'), with=FALSE], sep=',',  file='entrega_power_corta_6800.csv')
+entrega[  ,  estimulo :=  c(rep(1, 6900),rep(0,nrow(entrega)-6900))]
+fwrite( entrega[ ,  c('numero_de_cliente', 'estimulo'), with=FALSE], sep=',',  file='entrega_power_corta_6900.csv')
+entrega[  ,  estimulo :=  c(rep(1, 7000),rep(0,nrow(entrega)-7000))]
+fwrite( entrega[ ,  c('numero_de_cliente', 'estimulo'), with=FALSE], sep=',',  file='entrega_power_corta_7000.csv')
+entrega[  ,  estimulo :=  c(rep(1, 7100),rep(0,nrow(entrega)-7100))]
+fwrite( entrega[ ,  c('numero_de_cliente', 'estimulo'), with=FALSE], sep=',',  file='entrega_power_corta_7100.csv')
+entrega[  ,  estimulo :=  c(rep(1, 7200),rep(0,nrow(entrega)-7200))]
+fwrite( entrega[ ,  c('numero_de_cliente', 'estimulo'), with=FALSE], sep=',',  file='entrega_power_corta_7200.csv')
+entrega[  ,  estimulo :=  c(rep(1, 7300),rep(0,nrow(entrega)-7300))]
+fwrite( entrega[ ,  c('numero_de_cliente', 'estimulo'), with=FALSE], sep=',',  file='entrega_power_corta_7300.csv')
+entrega[  ,  estimulo :=  c(rep(1, 7400),rep(0,nrow(entrega)-7400))]
+fwrite( entrega[ ,  c('numero_de_cliente', 'estimulo'), with=FALSE], sep=',',  file='entrega_power_corta_7400.csv')
+entrega[  ,  estimulo :=  c(rep(1, 7500),rep(0,nrow(entrega)-7500))]
+fwrite( entrega[ ,  c('numero_de_cliente', 'estimulo'), with=FALSE], sep=',',  file='entrega_power_corta_7500.csv')
+entrega[  ,  estimulo :=  c(rep(1, 7600),rep(0,nrow(entrega)-7600))]
+fwrite( entrega[ ,  c('numero_de_cliente', 'estimulo'), with=FALSE], sep=',',  file='entrega_power_corta_7600.csv')
+entrega[  ,  estimulo :=  c(rep(1, 7700),rep(0,nrow(entrega)-7700))]
+fwrite( entrega[ ,  c('numero_de_cliente', 'estimulo'), with=FALSE], sep=',',  file='entrega_power_corta_7700.csv')
+entrega[  ,  estimulo :=  c(rep(1, 7800),rep(0,nrow(entrega)-7800))]
+fwrite( entrega[ ,  c('numero_de_cliente', 'estimulo'), with=FALSE], sep=',',  file='entrega_power_corta_7800.csv')
+entrega[  ,  estimulo :=  c(rep(1, 7900),rep(0,nrow(entrega)-7900))]
+fwrite( entrega[ ,  c('numero_de_cliente', 'estimulo'), with=FALSE], sep=',',  file='entrega_power_corta_7900.csv')
